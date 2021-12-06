f8059915f0d30ee052fdefb52b684b77bb9a4a30
Michael Fulton <fulto081@umn.edu>
2021-12-06 15:57:30 -0600

    execution works

--- src/rcvm_aqua_server.py
+++ src/rcvm_aqua_server.py
@@ -20,30 +20,35 @@ import xml.etree.ElementTree as ET
 from proteus.kineme import Kineme, KNode
 from proteus.srv import SymbolTrigger, SymbolDirectional, SymbolTarget
 
-# params = {}
-# params['mode'] = AutopilotModes.AP_GLOBAL_ANGLES_LOCAL_THRUST
-# rpc = RCVMPilotClient(params)
+rospy.init_node('aqua_rcvm_server', argv=None, anonymous=True)
+
+params = {}
+params['mode'] = AutopilotModes.AP_GLOBAL_ANGLES_LOCAL_THRUST
+rpc = RCVMPilotClient(params)
 
 rcvm_params = None
 symbols = None
 kinemes = None
 
 # Farms out the execution of the kineme to the appropriate function
-def service_cb(req, id, call_type):
-    if call_type == 'trigger':
-        return execute_trigger(req, id)
-    elif call_type == 'directional':
-        return execute_directional(req, id)
-    elif call_type == 'target':
-        return execute_target(req, id)
+def service_cb(req, kineme):
+    rospy.logdebug('Service callback for kineme %s'%(kineme.id))
+    if kineme.call_type == 'trigger':
+        return execute_trigger(req, kineme)
+    elif kineme.call_type == 'directional':
+        return execute_directional(req, kineme)
+    elif kineme.call_type == 'target':
+        return execute_target(req, kineme)
     else:
         return False
 
 # Executes kinemes which are trigger called, meaning that there's no information in the service call.
-def execute_trigger(req, id):
-    kineme = kinemes[id]
+def execute_trigger(req, kineme):
+    rospy.loginfo('Executing trigger kineme %s'%(kineme.id))
     for knode in kineme.knodes:
         rpc.do_relative_angle_change((knode.orientation.roll, knode.orientation.pitch, knode.orientation.yaw), rpc.current_depth, knode.position.x, knode.position.y, knode.duration.seconds, threshold=rcvm_params['angle_diff_threshold'])
+
+    return True
     
 # Executes kinemes which are directional called, meaning that there's directional information in the service call.
 def execute_directional(req, id):
@@ -55,7 +60,6 @@ def execute_target(req, id):
 
 
 if __name__ == '__main__':
-    rospy.init_node('aqua_rcvm_server', argv=None, anonymous=True)
     rospy.loginfo('Initializing the Aqua8 RCVM server')
 
     rcvm_params = rospy.get_param('rcvm/', 'relative')
@@ -106,6 +110,7 @@ if __name__ == '__main__':
                 k.set_call_type(symbols.get(s).get('call_type'))
                 break
 
+    print(kinemes)
     # Setup service calls
     for key, kineme in kinemes.items():
         service_class = None
@@ -118,7 +123,8 @@ if __name__ == '__main__':
         else:
             rospy.logwarn("Unexpected kineme call type %s"%(kineme.call_type))
 
-        rospy.Service('rcvm/'+kineme.name, service_class, lambda req: service_cb(req, key, kineme.call_type))
+        rospy.loginfo('Advertising a service for kineme %s at service endpoint: %s'%(kineme.id, 'rcvm/'+kineme.name))
+        rospy.Service('rcvm/'+kineme.name, service_class, lambda req, kineme=kineme: service_cb(req, kineme))
 
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
